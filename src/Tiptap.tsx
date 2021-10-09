20304f6898d637ef2b75fa3f7aa8da1c2898a452
Tekeste Kidanu <tekestek@amazon.com>
2021-10-08 21:39:14 -0400

    notes widget v1

---
+++ src/Tiptap.tsx
@@ -0,0 +1,41 @@
+import Placeholder from '@tiptap/extension-placeholder';
+import { useEditor, EditorContent } from '@tiptap/react';
+import { Content } from '@tiptap/core';
+import StarterKit from '@tiptap/starter-kit'
+import { h } from 'preact'
+import '!./styles.css';
+
+type TipTapProps = {
+  handleUpdate: Function,
+  content: Content,
+  canEdit: boolean
+}
+const Tiptap = ({handleUpdate, content, canEdit}: TipTapProps) => {
+  const editor = useEditor({
+    extensions: [
+      StarterKit,
+      Placeholder.configure({
+        placeholder: 'Start writing in Markdown',
+      })    ],
+    autofocus: true,
+    editorProps: {
+      editable: () => canEdit,
+      attributes: {
+        class: "inner-editor",
+      },
+    },
+    onUpdate({ editor }) {
+      const previewText = editor.getText().split("\n")[0].substring(0, 27);
+      handleUpdate(editor.getJSON(), previewText);
+    },
+    content
+  })
+
+  return (
+    <div class="element">
+      <EditorContent editor={editor} />
+    </div>
+  )
+}
+
+export default Tiptap;
--- src/ui.tsx
+++ src/ui.tsx
@@ -1,38 +1,103 @@
-import {
-  Button,
-  Container,
-  render,
-  TextboxMultiline,
-  useInitialFocus,
-  VerticalSpace
-} from '@create-figma-plugin/ui'
-import { emit } from '@create-figma-plugin/utilities'
-import { h } from 'preact'
-import { useCallback, useState } from 'preact/hooks'
+import { Button, render } from "@create-figma-plugin/ui";
+import { emit, on } from "@create-figma-plugin/utilities";
+import { Content } from "@tiptap/core";
+import { nanoid } from "nanoid";
+import { Fragment, h } from "preact";
+import { useState } from "preact/hooks";
+import { Note } from "./main";
+import Tiptap from "./TipTap";
 
-function Plugin (props: { text: string }) {
-  const [text, setText] = useState(props.text)
-  const handleUpdateButtonClick = useCallback(
-    function () {
-      emit('UPDATE_TEXT', text)
-    },
-    [text]
-  )
+function Plugin(props: { text: string }) {
+  const [activeNote, setActiveNote] = useState<Note>();
+  const [canEdit, setCanEdit] = useState(false);
+
+  const handleUpdate = (content: Content, previewText: string) => {
+    if (!activeNote) return;
+    setActiveNote({
+      ...activeNote,
+      content,
+      previewText,
+    });
+    emit("NOTE-UPDATED", {
+      ...activeNote,
+      content,
+      previewText: previewText.length >= 27 ? previewText + "..." : previewText,
+    });
+  };
+  on(
+    "UPDATE_OR_CREATE_NOTE",
+    ({
+      selectedNodeId,
+      currentUser,
+      note,
+    }: {
+      selectedNodeId: string;
+      currentUser: User;
+      note: Note;
+    }) => {
+      const noteId = nanoid();
+      const content = "";
+      const previewText = "";
+      let activeNote;
+      setCanEdit(false);
+      if (note) {
+        // we are editing an existing note
+        activeNote = note;
+        // check if currentUser is the same as activeUser
+        // to decide whether we should let them edit or not
+        setCanEdit(false);
+        if (note.creator.id === currentUser.id) {
+          setCanEdit(true);
+        }
+      } else {
+        // must be creating a new one
+        activeNote = {
+          noteId,
+          creator: currentUser,
+          selectedNodeId,
+          content,
+          previewText,
+          createdAt: Date.now(),
+        };
+        setCanEdit(true);
+      }
+      setActiveNote(activeNote);
+    }
+  );
+  const handleDone = () => {
+    emit("CLOSE_PLUGIN");
+  };
+  const handleDelete = () => {
+    if (!activeNote) return;
+    const message = confirm("Delete note?");
+    if (message) {
+      emit("DELETE_NOTE", activeNote?.noteId);
+    }
+  };
+
+  if (!activeNote) return null;
   return (
-    <Container>
-      <VerticalSpace space='large' />
-      <TextboxMultiline
-        {...useInitialFocus()}
-        onValueInput={setText}
-        value={text}
+    <Fragment>
+      <div class="info-bar">
+        {new Date(activeNote.createdAt).toLocaleDateString()} •{" "}
+        {activeNote.creator.name}
+      </div>
+      <Tiptap
+        handleUpdate={handleUpdate}
+        content={activeNote.content}
+        canEdit={canEdit}
       />
-      <VerticalSpace space='large' />
-      <Button fullWidth onClick={handleUpdateButtonClick}>
-        Update Text
-      </Button>
-      <VerticalSpace space='small' />
-    </Container>
-  )
+      <div class="action-bar">
+        <span class="notice">Notes get saved automatically.</span>
+        <Button class="save-btn" onClick={handleDone}>
+          Done
+        </Button>
+        <Button secondary destructive onClick={handleDelete}>
+          Delete
+        </Button>
+      </div>
+    </Fragment>
+  );
 }
 
-export default render(Plugin)
+export default render(Plugin);
